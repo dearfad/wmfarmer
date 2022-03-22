9af5c7a553ef1a0b7367b225f2ae6e3829ac1c5e
dearfad <dearfad@sina.com>
2022-03-22 20:08:20 +0800

    Update wmpl.py

--- wmpl.py
+++ wmpl.py
@@ -15,43 +15,32 @@ def get_order_info(item_name):
     'highest_buy_price': 0,
     'buy_user': '', 
     'query_time': 0,
+    'status': '',
   }
   requests_result = requests.get(f'https://api.warframe.market/v1/items/{item_name}/orders', headers={'Platform': 'pc'})
-  st.write(requests_result)
-  # payload = json.loads(result.text)
-  # orders = payload['payload']['orders']
-  # high_buy_price = 0
-  # buy_user = ''
-  # low_sell_price = 0
-  # sell_user = ''
-  # for order in orders:
-  #   if order['order_type']=='sell' and order['user']['status']=='ingame':
-  #     # print('sell', order['platinum'], order['user']['ingame_name'])
-  #     if low_sell_price==0:
-  #       low_sell_price = order['platinum']
-  #       sell_user = order['user']['ingame_name']
-  #     else:
-  #       if order['platinum']<low_sell_price:
-  #         low_sell_price = order['platinum']
-  #         sell_user = order['user']['ingame_name']
-  #   if order['order_type']=='buy' and order['user']['status']=='ingame':
-  #     # print('buy', order['platinum'], order['user']['ingame_name'])
-  #     if high_buy_price==0:
-  #       high_buy_price = order['platinum']
-  #       buy_user = order['user']['ingame_name']
-  #     else:
-  #       if order['platinum']>high_buy_price:
-  #         high_buy_price = order['platinum']
-  #         buy_user = order['user']['ingame_name']
-  # end_time = time.time()
-  # last_time = end_time-start_time
-  # time_text = 'time: '+ str(round(last_time, 3))
-  # st.text(time_text)
-  # st.text(name)
-  # low_price = '低卖：' + str(low_sell_price) + ' ' + sell_user
-  # st.text(low_price)
-  # high_price = '高买：' + str(high_buy_price) + ' ' + buy_user
-  # st.text(high_price)
+  order_info['status'] = request_result
+  if order_info['status'] == '<Response [200]>':
+    payload = json.loads(request_result.text)
+    orders = payload['payload']['orders']
+    for order in orders:
+      if order['user']['status']=='ingame':
+        if order['order_type']=='sell':
+          if order_info['lowest_sell_price']==0:
+            order_info['lowest_sell_price'] = order['platinum']
+            order_info['sell_user'] = order['user']['ingame_name']
+          else:
+            if order['platinum']<order_info['lowest_sell_price']:
+              order_info['lowest_sell_price'] = order['platinum']
+              order_info['sell_user'] = order['user']['ingame_name']
+        if order['order_type']=='buy':
+          if order_info['highest_buy_price']==0:
+            order_info['highest_buy_price'] = order['platinum']
+            order_info['buy_user'] = order['user']['ingame_name']
+          else:
+            if order['platinum']>order_info['highest_buy_price']:
+              order_info['highest_buy_price'] = order['platinum']
+              order_info['buy_user'] = order['user']['ingame_name']
+    order_info['query_time'] = str(round(time.time()-start_time,3))
   return order_info
 
 st.title('Warframe Market Price List')
