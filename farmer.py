807fea7de33f91bc91330ea65dc0335c05f6c8a0
dearfad <dearfad@sina.com>
2022-03-25 23:50:47 +0800

    Update farmer.py

--- farmer.py
+++ farmer.py
@@ -124,8 +124,8 @@ for i, col in enumerate(cols):
           st.button(str(i)+str(n))
   if i>10:
     with col:
-      for n in range(3):
-        st.button(str((i-10)*(n+1)))
+      for n in range(1,4):
+        st.button(str((i-10+n)))
         
 relic_prefix = {
     '古纪': 'lith',
