903fcee6c7565685d3b734b06349f2a630b36d8f
dearfad <dearfad@sina.com>
2022-03-25 19:25:44 +0800

    Update farmer.py

--- farmer.py
+++ farmer.py
@@ -53,7 +53,7 @@ def get_droptables():
 def get_url_names():
   url_names = {}
   r = requests.get('https://api.warframe.market/v1/items', headers={'Language': 'zh-hans'})
-  if r.statuscode == 200:
+  if r.status_code == 200:
       payload = result.json()
       items = payload['payload']['items']
       for item in items:
