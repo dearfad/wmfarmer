10fbc167d01e7267c9cfa04b376233c48880743b
dearfad <dearfad@sina.com>
2022-03-26 10:19:53 +0800

    Update farmer.py

--- farmer.py
+++ farmer.py
@@ -107,8 +107,8 @@ def get_order_info(item_name):
 st.title('Warframe Farmer')
   
 droptables = get_droptables()
-st.write(droptables)
 url_names = get_url_names()
+st.write(url_names)
 
 relic_type_col, relic_drop_col = st.columns([1,3])
 
