48607b896f522c0eb899a62d081bf52b363dc4fb
dearfad <dearfad@sina.com>
2022-03-26 10:31:20 +0800

    Update farmer.py

--- farmer.py
+++ farmer.py
@@ -125,9 +125,7 @@ relic_prefix = {
 with relic_drop_col:
     item_name = st.text_input('').lower()
     item_name = relic_prefix[relic_type]+ ' ' + item_name + ' relic'
-    st.write(item_name)
     drop_list = droptables['Relics'].get(item_name)
-    st.write(drop_list)
     if drop_list:
       df = pd.DataFrame()
       df['url_name'] = drop_list 
@@ -138,7 +136,7 @@ with relic_drop_col:
           item = item.strip('Blueprint').strip()
         url_name = item.replace(' ','_')
         cn_name = url_names.get(url_name.lower())
-        if url_name == 'Forma_Blueprint':
+        if url_name == 'forma_blueprint':
           cn_name = 'Forma 蓝图'
         drop_list_cn.append(cn_name)
         price = get_order_info(url_name.lower())['buy']
