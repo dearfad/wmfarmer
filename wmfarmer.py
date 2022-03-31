a3ed918a3bb429a0b1a46368e252317fb2d6b97b
dearfad <dearfad@sina.com>
2022-03-31 22:06:49 +0800

    Update wmfarmer.py

--- wmfarmer.py
+++ wmfarmer.py
@@ -64,12 +64,15 @@ def get_order_info(item_name):
 st.title('Warframe Market Farmer')
 item_name = st.text_input('名称：', 'Xiphos 机身')
 items = get_items()
-item = items[items['item_name_cn']==item_name].to_dict(orient='records')[0]
-thumb_url = assets_url + item['thumb']
+if items:
+  item = items[items['item_name_cn']==item_name].to_dict(orient='records')[0]
+  thumb_url = assets_url + item['thumb']
 
-order_info = get_order_info(item['url_name'])
+  order_info = get_order_info(item['url_name'])
 
-col0, col1, col2 = st.columns(3)
-col0.image(thumb_url)
-col1.metric("最高卖出", order_info['buy'], order_info['buyer'])
-col2.metric("最低买入", order_info['sell'], order_info['seller'])
+  col0, col1, col2 = st.columns(3)
+  col0.image(thumb_url)
+  col1.metric("最高卖出", order_info['buy'], order_info['buyer'])
+  col2.metric("最低买入", order_info['sell'], order_info['seller'])
+else:
+  st.write('未找到相关信息...')
