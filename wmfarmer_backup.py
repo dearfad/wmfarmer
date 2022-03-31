68d525d8484e96ca213852556f8ff46f748fffad
dearfad <dearfad@sina.com>
2022-03-31 20:11:28 +0800

    Update and rename wmfarmer.py to wmfarmer_backup.py

--- wmfarmer_backup.py
+++ wmfarmer_backup.py
@@ -178,17 +178,17 @@ for x, y in enumerate(names):
   with relic_drop_col[x]:
     show_price(y)
 
-nightmaremoderewards = ['Ice Storm','Stunning Speed','Hammer Shot','Wildfire','Accelerated Blast','Blaze','Chilling Reload','Drifting Contact','Seeking Fury','Armored Agility','Shred','Rending Strike','Fortitude','Streamlined Form','Animal Instinct','Vigor','Lethal Torrent','Focus Energy','Constitution']
+# nightmaremoderewards = ['Ice Storm','Stunning Speed','Hammer Shot','Wildfire','Accelerated Blast','Blaze','Chilling Reload','Drifting Contact','Seeking Fury','Armored Agility','Shred','Rending Strike','Fortitude','Streamlined Form','Animal Instinct','Vigor','Lethal Torrent','Focus Energy','Constitution']
 
-night = pd.DataFrame()
-night['url_name'] = [x.replace(' ','_').lower() for x in nightmaremoderewards]
-night['中文'] = [url_names.get(x) for x in night['url_name']]
-night['价格'] = [get_order_info(x)['buy'] for x in night['url_name']]
-r_a = '水星 金星 地球 火星'
-r_b = '火卫一 谷神星 木星 欧罗巴 土星 月球 虚空 赤毒要塞 火卫二'
-r_c = '天王星 海王星 冥王星 阋神星 赛德娜'
-night['位置'] = [r_a,r_a,r_a,r_a,r_a,r_a,r_a,r_b,r_b,r_b,r_b,r_b,r_b,r_c,r_c,r_c,r_c,r_c,r_c,]
-st.table(night)
+# night = pd.DataFrame()
+# night['url_name'] = [x.replace(' ','_').lower() for x in nightmaremoderewards]
+# night['中文'] = [url_names.get(x) for x in night['url_name']]
+# night['价格'] = [get_order_info(x)['buy'] for x in night['url_name']]
+# r_a = '水星 金星 地球 火星'
+# r_b = '火卫一 谷神星 木星 欧罗巴 土星 月球 虚空 赤毒要塞 火卫二'
+# r_c = '天王星 海王星 冥王星 阋神星 赛德娜'
+# night['位置'] = [r_a,r_a,r_a,r_a,r_a,r_a,r_a,r_b,r_b,r_b,r_b,r_b,r_b,r_c,r_c,r_c,r_c,r_c,r_c,]
+# st.table(night)
 
 # warframe_prime_set_list = ['set', 'blueprint', 'neuroptics', 'chassis', 'systems']
 
