d4fb76294b2f927df480665ca5db465d564d1125
dearfad <dearfad@sina.com>
2022-03-31 20:17:04 +0800

    Update wmfarmer.py

--- wmfarmer.py
+++ wmfarmer.py
@@ -8,14 +8,14 @@ def get_items():
   r_en = requests.get(items_api_url, headers={"Language": "en"})
 
   if r_en.status_code == 200:
-      items_en = pd.json_normalize(r_en.json()["payload"]["items"])
-      items_en.rename(columns={"item_name": "item_name_en"}, inplace=True)
+    items_en = pd.json_normalize(r_en.json()["payload"]["items"])
+    items_en.rename(columns={"item_name": "item_name_en"}, inplace=True)
 
   r_cn = requests.get(items_api_url, headers={"Language": "zh-hans"})
 
   if r_cn.status_code == 200:
-      items_cn = pd.json_normalize(r_cn.json()["payload"]["items"])
-      items_cn.rename(columns={"item_name": "item_name_cn"}, inplace=True)
+    items_cn = pd.json_normalize(r_cn.json()["payload"]["items"])
+    items_cn.rename(columns={"item_name": "item_name_cn"}, inplace=True)
 
   items = pd.merge(items_cn, items_en)
 
