fd497cc14a71dc0b8233ace4a1cb43471ef05b3d
dearfad <dearfad@sina.com>
2022-04-01 12:32:52 +0800

    Update wmfarmer.py

--- wmfarmer.py
+++ wmfarmer.py
@@ -149,10 +149,9 @@ def relic():
   for key in droptables['relics']:
     if item_name.capitalize() in key:
       search_result.append(key)
-  st.write(search_result)
-#   item_names = search_result['item_name_cn'].values
-#   selected_name = st.selectbox('已发现：', item_names)
-#   item_df = items[items['item_name_cn']==selected_name]
+  selected_name = st.selectbox('已发现：', search_result)
+  item_df = items[items['item_name_en']==selected_name]
+  show_item(item_df)
 # relic_prefix = {
 #     '古纪': 'lith',
 #     '前纪': 'meso',
@@ -161,30 +160,7 @@ def relic():
 #     '安魂': 'requiem',
 # }
 
-# def show_price(item_name):
-# # with relic_drop_col[0]:
-# #     item_name = st.text_input('').lower()
-#     item_name = relic_prefix[relic_type]+ ' ' + item_name + ' relic'
-#     drop_list = droptables['relics'].get(item_name)
-#     if drop_list:
-#       df = pd.DataFrame()
-#       df['url_name'] = drop_list 
-#       drop_list_cn = []
-#       price_list = []
-#       for item in drop_list:
-#         if 'neuroptics' in item or 'chassis' in item or 'systems' in item:
-#           if item.split(' ')[0] in warframe_prime_list:
-#             item = item[:-10]
-#         url_name = item.replace(' ','_')
-#         cn_name = url_names.get(url_name.lower())
-#         if url_name == 'forma_blueprint':
-#           cn_name = 'Forma 蓝图'
-#         drop_list_cn.append(cn_name)
-#         price = get_order_info(url_name.lower())['buy']
-#         price_list.append(price)
-#       df['中文'] = drop_list_cn
-#       df['价格'] = price_list
-#       st.table(df[['中文','价格']])
+
   return
 
 def main():
