4e70bea43f62e87c4ba4a680e42499abce1d5825
dearfad <dearfad@sina.com>
2022-04-01 10:24:43 +0800

    Update wmfarmer.py

--- wmfarmer.py
+++ wmfarmer.py
@@ -77,8 +77,8 @@ def nightmare():
 def warframe():
   warframe_prime_list = ['ash', 'atlas', 'banshee', 'chroma', 'ember', 'equinox', 'frost', 'gara', 'harrow', 'hydroid', 'inaros', 'ivara', 'limbo', 'loki', 'mag', 'mesa', 'mirage', 'nekros', 'nezha', 'nidus', 'nova', 'nyx', 'oberon', 'octavia', 'rhino', 'saryn', 'titania', 'trinity', 'valkyr', 'vauban', 'volt', 'wukong', 'zephyr']
   warframe_prime_set_list = ['set', 'blueprint', 'neuroptics', 'chassis', 'systems']
-  price_df = pd.DataFrame(columns = ['name', 'sell', 'seller', 'buy', 'buyer', 'time', 'status'])
-  warframe_selection = st.sidebar.selectbox('选择战甲', warframe_prime_list)
+  price_df = pd.DataFrame(columns = ['name', 'sell', 'seller', 'buy', 'buyer', 'status'])
+  warframe_selection = st.selectbox('选择战甲', warframe_prime_list)
   with st.empty():
     for item in warframe_prime_set_list:
       item_name = warframe_selection + '_prime_' + item
