c40395c389dbd10d715c80220c96dcbe9751ba51
dearfad <dearfad@sina.com>
2022-04-06 19:30:45 +0800

    Update wmfarmer.py

--- wmfarmer.py
+++ wmfarmer.py
@@ -29,9 +29,9 @@ def get_order_info(url_name):
   r = requests.get(f'{items_api_url}/{url_name}/orders', headers={'Platform': 'pc'})  
   if r.status_code == 200:
       order_info['status'] = 'T'
-      utc_time = datetime.utcnow()
-#       utc_time = datetime.utcnow().strftime("%H:%M:%S")
-      order_info['time'] = utc_time.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
+      utc_time = datetime.utcnow().strftime("%H:%M:%S")
+      st.write('utc_time', utc_time)
+#       order_info['time'] = utc_time.replace(tzinfo=datetime.timezone.utc).astimezone(tz=None)
       orders = r.json()['payload']['orders']
       for order in orders:
         if order['user']['status']=='ingame':
