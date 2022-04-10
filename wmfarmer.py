2800a04bcf17c67a9a6c55ec06fbc6bafeeb570d
dearfad <dearfad@sina.com>
2022-04-10 09:28:59 +0800

    Update wmfarmer.py

--- wmfarmer.py
+++ wmfarmer.py
@@ -7,9 +7,9 @@ st.set_page_config(page_title='Warframe Market Farmer', page_icon='👨‍🌾')
 def main():
     pages = {
         '物品价格': item.page,
-        '战甲套装': warframe.page,
-        '噩梦收益': nightmare.page,
-        '虚空裂缝': relic.page
+        # '战甲套装': warframe.page,
+        # '噩梦收益': nightmare.page,
+        # '虚空裂缝': relic.page
     }
     with st.sidebar:
         st.title('Warframe Market Farmer')
