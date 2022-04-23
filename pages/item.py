f530335025bee6c31dbe7bf310911f804322fce7
dearfad <dearfad@sina.com>
2022-04-23 16:07:52 +0800

    Update item.py

--- pages/item.py
+++ pages/item.py
@@ -11,7 +11,7 @@ def page():
 
     items, item_info = data_init()
 
-    col0, col1, col2 = st.columns(3)
+    col0, col1, col2 = st.columns([2,1,1])
 
     with col0:
         input_name = st.text_input('模糊搜索：', '')
