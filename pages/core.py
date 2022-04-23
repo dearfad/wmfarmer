f975129a5ae12bd9b7d6c80aa8b9fd197c8f6e22
dearfad <dearfad@sina.com>
2022-04-23 11:09:22 +0800

    Update core.py

--- pages/core.py
+++ pages/core.py
@@ -10,7 +10,14 @@ def get_time():
     time = utc_time.astimezone(
         timezone(timedelta(hours=8))).strftime("%Y-%m-%d %H:%M:%S")
     return time
-    
+
+def markdown_list(itemlist):
+    markdown = "- "
+    if itemlist:
+        markdown = '- \n'.join(itemlist)
+    return markdown
+
+
 
 def show_item(url_name):
     item_info = get_item_info(url_name)
@@ -26,7 +33,7 @@ def show_item(url_name):
     col0.write(
         f"[![{item_info['zh-hans']['item_name']}]({assets_url+item_info['thumb']})]({item_info['zh-hans']['wiki_link']})")
     col0.write(f"![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info.get('ducats', '--')}**")
-    col1.write(item_info['tags'])
+    col1.write(markdown_list(item_info['tags']))
     # col1.bar_chart(item_orders[item_orders['order_type']=='buy']['platinum'], height=160, use_container_width=True)
     # col2.write('col2')
     # col1.metric("最高卖出", item_orders['buy'], item_orders['buyer'])
