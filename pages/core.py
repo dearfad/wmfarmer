bcd7bafe03791e10fb2bd9348a99ae4db94d9c88
dearfad <dearfad@sina.com>
2022-04-23 18:35:52 +0800

    Update core.py

--- pages/core.py
+++ pages/core.py
@@ -51,13 +51,13 @@ def show_item_orders(orders):
         st.write(f"当前最高买价：{orders_dict['ingame_highest_buy_platinum']}")
         st.write(f"当前最高买者：{orders_dict['ingame_highest_buyer']}")
         with st.expander('买单列表'):
-            if not orders_dict['ingame_high_buy'].empty:
+            if orders_dict['ingame_high_buy']:
                 st.write(orders_dict['ingame_high_buy'][['platinum','last_update','user.ingame_name']])
         st.write(f"当前最低卖价：{orders_dict['ingame_lowest_sell_platinum']}")
         st.write(f"当前最低卖者：{orders_dict['ingame_lowest_seller']}")
         with st.expander('卖单列表'):
             st.write(orders_dict['ingame_low_sell'])
-            if not orders_dict['ingame_low_sell'].empty:
+            if orders_dict['ingame_low_sell']:
                 st.write(orders_dict['ingame_low_sell'][['platinum','last_update','user.ingame_name']])
     return
 
