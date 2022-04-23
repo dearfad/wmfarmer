3b72b6bf34375e8277ffa78e5170fe7026388c47
dearfad <dearfad@sina.com>
2022-04-23 16:03:31 +0800

    Update item.py

--- pages/item.py
+++ pages/item.py
@@ -15,15 +15,14 @@ def page():
 
     with col0:
         input_name = st.text_input('模糊搜索：', '')
-        # items, time = get_items()
-        # search_result = items[items['item_name'].str.contains(
-        #     input_name.strip(), case=False)]
-        # if search_result.empty:
-        #     st.warning('未找到相关信息...')
-        # else:
-        #     selected_name = st.selectbox(
-        #         '已发现：', search_result['item_name'] + ' ' + search_result['url_name'])
-        #     url_name = selected_name.split(' ')[-1]
+        search_result = items['item'][items['item']['item_name'].str.contains(
+            input_name.strip(), case=False)]
+        if search_result.empty:
+            st.warning('未找到相关信息...')
+        else:
+            selected_name = st.selectbox(
+                '已发现：', search_result['item_name'] + ' ' + search_result['url_name'])
+            url_name = selected_name.split(' ')[-1]
     
     with col1:
         st.write(f"- 获取列表时间: {items['time']}")
