3693d58ffc2bbf669e6f9de2aace773817058673
dearfad <dearfad@sina.com>
2022-04-23 12:04:31 +0800

    update

--- pages/core.py
+++ pages/core.py
@@ -23,21 +23,25 @@ def markdown_list(itemlist):
 def show_item(url_name):
     item_info = get_item_info(url_name)
     st.write(item_info)
-    item_orders = ''
-    if get_item_orders(url_name):
-        item_orders = pd.json_normalize(get_item_orders(url_name))
+
+    # item_orders = ''
+    # if get_item_orders(url_name):
+    #     item_orders = pd.json_normalize(get_item_orders(url_name))
+
     # st.write(item_orders)
     # orders = fmt_item_orders(item_orders)
     # st.write(orders)
     # st.write(orders.columns)
-    st.write(
-        f"**{item_info['zh-hans']['item_name']}** 📝 {item_info['zh-hans']['description']}")
-    col0, col1 = st.columns([1, 4])
-    col0.write(
-        f"[![{item_info['zh-hans']['item_name']}]({assets_url+item_info['thumb']})]({item_info['zh-hans']['wiki_link']})")
-    col0.write(
-        f"![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info.get('ducats', '--')}**")
-    col1.write(f"- {item_info['url_name']}\n{markdown_list(item_info['tags'])}")
+    
+    # st.write(
+    #     f"**{item_info['zh-hans']['item_name']}** 📝 {item_info['zh-hans']['description']}")
+    # col0, col1 = st.columns([1, 4])
+    # col0.write(
+    #     f"[![{item_info['zh-hans']['item_name']}]({assets_url+item_info['thumb']})]({item_info['zh-hans']['wiki_link']})")
+    # col0.write(
+    #     f"![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info.get('ducats', '--')}**")
+    # col1.write(f"- {item_info['url_name']}\n{markdown_list(item_info['tags'])}")
+
     # col1.bar_chart(item_orders[item_orders['order_type']=='buy']['platinum'], height=160, use_container_width=True)
     # col2.write('col2')
     # col1.metric("最高卖出", item_orders['buy'], item_orders['buyer'])
