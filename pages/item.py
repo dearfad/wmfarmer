764d501a39ab718fa61c1eefa31ce519a8e7adf4
dearfad <dearfad@sina.com>
2022-04-23 14:59:41 +0800

    Update item.py

--- pages/item.py
+++ pages/item.py
@@ -4,16 +4,19 @@ from pages.core import show_item
 
 
 def page():
-    
-    input_name = st.text_input('模糊搜索：', '')
-    items, time = get_items()
-    st.write(time)
-    search_result = items[items['item_name'].str.contains(
-        input_name.strip(), case=False)]
-    if search_result.empty:
-        st.warning('未找到相关信息...')
-    else:
-        selected_name = st.selectbox(
-            '已发现：', search_result['item_name'] + ' ' + search_result['url_name'])
-        url_name = selected_name.split(' ')[-1]
-        show_item(url_name)
+
+    col0, col1 = st.columns(2)
+
+    with col0:
+        input_name = st.text_input('模糊搜索：', '')
+        items, time = get_items()
+        st.write(time)
+        search_result = items[items['item_name'].str.contains(
+            input_name.strip(), case=False)]
+        if search_result.empty:
+            st.warning('未找到相关信息...')
+        else:
+            selected_name = st.selectbox(
+                '已发现：', search_result['item_name'] + ' ' + search_result['url_name'])
+            url_name = selected_name.split(' ')[-1]
+            show_item(url_name)
