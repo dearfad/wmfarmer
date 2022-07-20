84d5b3624f5f70b1cf06eaa84db9e395c8e77ce9
dearfad <dearfad@sina.com>
2022-07-20 21:58:22 +0800

    update

--- pages/2_warframe.py
+++ pages/2_warframe.py
@@ -2,7 +2,7 @@ import streamlit as st
 from scripts.wmmarket import item_orders
 from scripts.core import item_price
 
-warframe_prime_list = ['ash', 'atlas', 'banshee', 'chroma', 'ember', 'equinox', 'frost', 'gara', 'garuda', 'harrow', 'hydroid', 'inaros', 'ivara', 'limbo', 'loki', 'mag',
+warframe_prime_list = ['ash', 'atlas', 'banshee', 'chroma', 'ember', 'equinox', 'frost', 'gara', 'garuda', 'harrow', 'hydroid', 'inaros', 'ivara', 'khora', 'limbo', 'loki', 'mag',
                            'mesa', 'mirage', 'nekros', 'nezha', 'nidus', 'nova', 'nyx', 'oberon', 'octavia', 'rhino', 'saryn', 'titania', 'trinity', 'valkyr', 'vauban', 'volt', 'wukong', 'zephyr']
 warframe_prime_set_list = ['set', 'blueprint', 'neuroptics', 'chassis', 'systems']
 
