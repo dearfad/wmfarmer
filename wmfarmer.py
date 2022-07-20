40f0cd5543e1563ffe4edde4bf652fecfb240940
dearfad <dearfad@sina.com>
2022-07-20 14:46:55 +0800

    update

--- wmfarmer.py
+++ wmfarmer.py
@@ -6,4 +6,4 @@ st.set_page_config(page_title='Warframe Market Farmer', page_icon='👨‍🌾')
 st.write("# Warframe Market Farmer! 👋")
 
 st.write(f"- {items()['time']}")
-st.write(f"- Total items: {items()['items'].shape[0]}")
+st.write(f"- Total items: **{items()['items'].shape[0]}**")
