a176086bcc816331a2332fb8ef04571fca34ae3a
dearfad <dearfad@sina.com>
2022-07-20 22:07:54 +0800

    update

--- pages/2_warframe.py
+++ pages/2_warframe.py
@@ -10,7 +10,7 @@ warframe_prime_set_list = ['set', 'blueprint', 'neuroptics', 'chassis', 'systems
 for warframe in warframe_prime_list:
     cols = st.columns(len(warframe_prime_set_list)+1)
     with cols[0]:
-        st.write(warframe)
+        st.write(f"**{warframe}**")
     for i, item in enumerate(warframe_prime_set_list):
         with cols[i+1]:
             url_name = warframe + '_prime_' + item
