46ec3644eeefeb5c5a054ee1776bbdd5af0e8c5c
dearfad <dearfad@sina.com>
2022-07-20 16:42:10 +0800

    update

--- wmfarmer.py
+++ wmfarmer.py
@@ -2,11 +2,11 @@ import streamlit as st
 from scripts.wmmarket import items
 
 st.set_page_config(page_title='Warframe Market Farmer', page_icon='👨‍🌾')
-st.write("#          👨‍🌾")
+
 st.write("# Warframe Market Farmer! 👨‍🌾")
 
 if items()['time']=='failed':
-    st.write(f"- **Failed!**")
-    st.write(f"- **Status Code**: **{items()[items]}**")
+    st.write(f"➖ ⏱️ **Failed** ➖ *️⃣ **Status Code: {items()[items]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
 else:
     st.write(f"➖ ⏱️ **{items()['time']}** ➖ *️⃣ **Total: {items()['items'].shape[0]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
+    st.write(f"➖ ⏱️ **Failed** ➖ *️⃣ **Status Code: {items()[items]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
