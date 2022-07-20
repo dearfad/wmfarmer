408dd6e1938a731bff156ff5f0e51be4feecd4c2
dearfad <dearfad@sina.com>
2022-07-20 14:34:48 +0800

    update

--- wmfarmer.py
+++ wmfarmer.py
@@ -1,9 +1,8 @@
 import streamlit as st
-from scripts.wmmarket import items, get_item_info, get_item_orders
+from scripts.wmmarket import items
 
 st.set_page_config(page_title='Warframe Market Farmer', page_icon='👨‍🌾')
 
 st.write("# Warframe Market Farmer! 👋")
 
-items = items()
-st.write(f"- 获取列表时间: {items['time']}")
+st.write(f"- 获取列表时间: {items()['time']}")
