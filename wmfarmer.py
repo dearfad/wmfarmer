b6b225a616c4c6b60b36a1882e55636085ab44c4
dearfad <dearfad@sina.com>
2022-07-20 16:45:22 +0800

    update

--- wmfarmer.py
+++ wmfarmer.py
@@ -6,8 +6,6 @@ st.set_page_config(page_title='Warframe Market Farmer', page_icon='👨‍🌾')
 st.write("# Warframe Market Farmer! 👨‍🌾")
 
 if items()['time']=='failed':
-    st.write(f"➖ ⏱️ **Failed** ➖ *️⃣ **Status Code: {items()[items]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
+    st.write(f"➖ ⏱️ **Get Items Failed** ➖ *️⃣ **Status Code: {items()[items]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
 else:
     st.write(f"➖ ⏱️ **{items()['time']}** ➖ *️⃣ **Total: {items()['items'].shape[0]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
-    st.write(f"➖ ⏱️ **Get Items Failed** ➖ *️⃣ **Status Code: 200** ➖ 👨‍💼 **By: DEARFAD** ➖")
-    # st.write(f"➖ ⏱️ **Get Items Failed** ➖ *️⃣ **Status Code: {items()[items]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
