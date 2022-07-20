18804de787793cd82157ad291aed2239e38c5410
dearfad <dearfad@sina.com>
2022-07-20 17:33:06 +0800

    update

--- pages/1_Search.py
+++ pages/1_Search.py
@@ -5,18 +5,18 @@ from scripts.wmmarket import items, item_info, item_orders
 
 items = items()['items']
 
-search_col, empty_col = st.columns([1, 1])
+# search_col, empty_col = st.columns([1, 1])
 
-with search_col:
-    input_name = st.text_input('模糊搜索：', '')
-    search_result = items[items['item_name'].str.contains(input_name.strip(), case=False)]
-    if search_result.empty:
-        st.warning('未找到相关信息...')
-    else:
-        selected_name = st.selectbox('已发现：', search_result['item_name'])
-        url_name = search_result[search_result['item_name']==selected_name]['url_name'].values[0]
-        item_info = item_info(url_name)
-        item_orders = item_orders(url_name)
-        st.write(f"➖ ⏱️ **Info: {item_info['time'].split()[1]}** ➖ ⏲️ **Orders: {item_orders['time'].split()[1]}** ➖")
+# with search_col:
+input_name = st.text_input('模糊搜索：', '')
+search_result = items[items['item_name'].str.contains(input_name.strip(), case=False)]
+if search_result.empty:
+    st.warning('未找到相关信息...')
+else:
+    selected_name = st.selectbox('已发现：', search_result['item_name'])
+    url_name = search_result[search_result['item_name']==selected_name]['url_name'].values[0]
+    item_info = item_info(url_name)
+    item_orders = item_orders(url_name)
+    st.write(f"➖ ⏱️ **Info: {item_info['time'].split()[1]}** ➖ ⏲️ **Orders: {item_orders['time'].split()[1]}** ➖")
     
 # show_item(url_name, item_info['info'], item_orders['orders'])
