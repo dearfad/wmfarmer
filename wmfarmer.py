c971ca9057a32d52c1ad0830f70433d377a40080
dearfad <dearfad@sina.com>
2022-07-20 13:50:39 +0800

    update

--- wmfarmer.py
+++ wmfarmer.py
@@ -1,21 +1,24 @@
 import streamlit as st
-from pages import item
+# from pages import item
 
-st.set_page_config(page_title='Warframe Market Farmer', page_icon='👨‍🌾')
-
-
-def main():
-    pages = {
-        '物品价格': item.page,
-        # '战甲套装': warframe.page,
-        # '噩梦收益': nightmare.page,
-        # '虚空裂缝': relic.page
-    }
-    with st.sidebar:
-        st.title('Warframe Market Farmer')
-        page = st.radio("请选择：", pages.keys())
-    pages[page]()
+st.set_page_config(
+    page_title='Warframe Market Farmer', 
+    page_icon='👨‍🌾'
+    )
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     pages = {
+#         '物品价格': item.page,
+#         # '战甲套装': warframe.page,
+#         # '噩梦收益': nightmare.page,
+#         # '虚空裂缝': relic.page
+#     }
+#     with st.sidebar:
+#         st.title('Warframe Market Farmer')
+#         page = st.radio("请选择：", pages.keys())
+#     pages[page]()
+
+
+# if __name__ == '__main__':
+#     main()
