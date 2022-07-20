98a2107a76ee04e6f3e364e7f19a04dedc30979a
dearfad <dearfad@sina.com>
2022-07-20 22:04:06 +0800

    update

--- pages/2_warframe.py
+++ pages/2_warframe.py
@@ -8,6 +8,8 @@ warframe_prime_set_list = ['set', 'blueprint', 'neuroptics', 'chassis', 'systems
 
 
 for warframe in warframe_prime_list:
+    cols = st.columns(len(warframe_prime_set_list))
+    st.write(cols)
     for i, item in enumerate(warframe_prime_set_list):
         item_name = warframe + '_prime_' + item
         st.write(i, item_name)
