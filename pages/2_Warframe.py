a084d596753e8518ffcbd959dd58b1bc8d3109bf
dearfad <dearfad@sina.com>
2022-07-21 09:11:38 +0800

    update

--- pages/2_Warframe.py
+++ pages/2_Warframe.py
@@ -1,32 +1,5 @@
 import streamlit as st
-from scripts.wmmarket import item_orders, item_info
-from scripts.core import item_price
-import pandas as pd
-import numpy as np
+from scripts.core import get_warframe_price
 
-warframe_prime_list = ['ash', 'atlas', 'banshee', 'chroma', 'ember', 'equinox', 'frost', 'gara', 'garuda', 'harrow', 'hydroid', 'inaros', 'ivara', 'khora', 'limbo', 'loki', 'mag',
-                           'mesa', 'mirage', 'nekros', 'nezha', 'nidus', 'nova', 'nyx', 'oberon', 'octavia', 'rhino', 'saryn', 'titania', 'trinity', 'valkyr', 'vauban', 'volt', 'wukong', 'zephyr']
-warframe_prime_set_list = ['set', 'blueprint', 'neuroptics', 'chassis', 'systems']
-
-
-warframe_df = pd.DataFrame(data=np.zeros((len(warframe_prime_list),len(warframe_prime_set_list)), dtype = int), index=warframe_prime_list, columns=warframe_prime_set_list)
-
-
-for warframe in warframe_prime_list:
-    for item in warframe_prime_set_list:
-        url_name = warframe + '_prime_' + item
-        if warframe=='khora':
-            if item in ['neuroptics', 'chassis', 'systems']:
-                url_name = url_name + '_blueprint'
-        orders = item_orders(url_name)
-        price = item_price(orders['orders'])
-        info = item_info(url_name)
-
-        warframe_df.loc[warframe, item] = price['ingame_lowest_sell_platinum']
-
-        # st.write(f"**{price['ingame_lowest_sell_platinum']}**")
-        # ducats = int(info['info'].get('ducats', '--'))
-        # label = "💛" if ducats==100 else ""
-        # st.write(f"**{price['ingame_highest_buy_platinum']}** {label}")
-
-st.write(warframe_df)
+warframe_price_df = get_warframe_price()
+st.write(warframe_price_df)
--- scripts/core.py
+++ scripts/core.py
@@ -1,3 +1,6 @@
+from scripts.wmmarket import item_orders, item_info
+from scripts.core import item_price
+import numpy as np
 import pandas as pd
 
 def item_price(orders):
@@ -20,3 +23,32 @@ def item_price(orders):
         item_price['ingame_lowest_sell_platinum'] = ingame_sell_orders.iloc[0].at['platinum']
 
     return item_price
+
+@st.cache(show_spinner=False, suppress_st_warning=True, ttl=86400.0)
+def get_warframe_price():
+
+    warframe_prime_list = ['ash', 'atlas', 'banshee', 'chroma', 'ember', 'equinox', 'frost', 'gara', 'garuda', 'harrow', 'hydroid', 'inaros', 'ivara', 'khora', 'limbo', 'loki', 'mag',
+                            'mesa', 'mirage', 'nekros', 'nezha', 'nidus', 'nova', 'nyx', 'oberon', 'octavia', 'rhino', 'saryn', 'titania', 'trinity', 'valkyr', 'vauban', 'volt', 'wukong', 'zephyr']
+    warframe_prime_set_list = ['set', 'blueprint', 'neuroptics', 'chassis', 'systems']
+
+    warframe_price_df = pd.DataFrame(data=np.zeros((len(warframe_prime_list),len(warframe_prime_set_list)), dtype = int), index=warframe_prime_list, columns=warframe_prime_set_list)
+
+
+    for warframe in warframe_prime_list:
+        for item in warframe_prime_set_list:
+            url_name = warframe + '_prime_' + item
+            if warframe=='khora':
+                if item in ['neuroptics', 'chassis', 'systems']:
+                    url_name = url_name + '_blueprint'
+            orders = item_orders(url_name)
+            price = item_price(orders['orders'])
+            info = item_info(url_name)
+
+            warframe_df.loc[warframe, item] = price['ingame_lowest_sell_platinum']
+
+            # st.write(f"**{price['ingame_lowest_sell_platinum']}**")
+            # ducats = int(info['info'].get('ducats', '--'))
+            # label = "💛" if ducats==100 else ""
+            # st.write(f"**{price['ingame_highest_buy_platinum']}** {label}")
+    
+    return warframe_price_df
