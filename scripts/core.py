7276d7c4ebbb5654e8053da1cb9871f4cfcd27a5
dearfad <dearfad@sina.com>
2022-07-21 09:26:22 +0800

    update

--- scripts/core.py
+++ scripts/core.py
@@ -34,9 +34,10 @@ def get_warframe_price():
     warframe_price_df = pd.DataFrame(data=np.zeros((len(warframe_prime_list),len(warframe_prime_set_list)), dtype = int), index=warframe_prime_list, columns=warframe_prime_set_list)
 
     warframe_progress = st.progress(0)
+    warframe_count = len(warframe_prime_list)
 
     for i, warframe in enumerate(warframe_prime_list):
-        warframe_progress.progress(i)
+        warframe_progress.progress(int(i/warframe_count*100))
         for item in warframe_prime_set_list:
             url_name = warframe + '_prime_' + item
             if warframe=='khora':
