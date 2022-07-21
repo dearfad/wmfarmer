db71f3f7325742e7e67b2f462a529a7d9166005a
dearfad <dearfad@sina.com>
2022-07-21 09:55:31 +0800

    update

--- pages/2_Warframe.py
+++ pages/2_Warframe.py
@@ -5,13 +5,18 @@ warframe_price_df = get_warframe_price()
 
 wcol1 = warframe_price_df.iloc[:10, :]
 wcol2 = warframe_price_df.iloc[10:20, :]
-wcol3 = warframe_price_df.iloc[20:, :]
+wcol3 = warframe_price_df.iloc[20:30, :]
+wcol4 = warframe_price_df.iloc[30:, :]
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 with col1:
     st.write(wcol1)
 with col2:
     st.write(wcol2)
+
+col3, col4 = st.columns(2)
 with col3:
     st.write(wcol3)
+with col4:
+    st.write(wcol4)
