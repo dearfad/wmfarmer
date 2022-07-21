87f8e191c1d020406516b9562f8f748f71d04a1f
dearfad <dearfad@sina.com>
2022-07-21 09:36:12 +0800

    update

--- scripts/core.py
+++ scripts/core.py
@@ -36,7 +36,7 @@ def get_warframe_price():
     progress = st.empty()
 
     for i, warframe in enumerate(warframe_prime_list):
-        progress.text(f"**{warframe.upper()}**")
+        progress.write(f"**{warframe.upper()}**")
         for item in warframe_prime_set_list:
             url_name = warframe + '_prime_' + item
             if warframe=='khora':
