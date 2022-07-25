096fa3dc2713097706fc1b94fb7d9abf1a78c4a2
dearfad <dearfad@sina.com>
2022-07-25 16:22:43 +0800

    update

--- wmfarmer.py
+++ wmfarmer.py
@@ -1,3 +1,4 @@
+from pandas.core import series
 import streamlit as st
 from scripts.wmmarket import get_items, get_item_info, get_item_orders
 from scripts.core import get_item_price
@@ -16,14 +17,16 @@ if items['time']=='failed':
 else:
     st.write(f"➖ ⏱️ **{items['time']}** ➖ *️⃣ **Total: {items['items'].shape[0]}** ➖ 👨‍💼 **By: DEARFAD** ➖")
 
-input_name = st.text_input('模糊搜索：', '')
-search_result = items['items'][items['items']['item_name'].str.contains(input_name.strip(), case=False)]
+search_col, empty_col, info_col = st.columns([3,1,3])
 
-if search_result.empty:
-    st.warning('未找到相关信息...')
-else:    
-    selected_name = st.selectbox('已发现：', search_result['item_name'])
-    url_name = search_result[search_result['item_name']==selected_name]['url_name'].values[0]
-    item_info = get_item_info(url_name)
-    item_orders = get_item_orders(url_name)
-    st.write(f"➖ ⏱️ **Info: {item_info['time'].split()[1]}** ➖ ⏲️ **Orders: {item_orders['time'].split()[1]}** ➖")
+with search_col:
+    input_name = st.text_input('模糊搜索：', '')
+    search_result = items['items'][items['items']['item_name'].str.contains(input_name.strip(), case=False)]
+    if search_result.empty:
+        st.warning('未找到相关信息...')
+    else:    
+        selected_name = st.selectbox('已发现：', search_result['item_name'])
+        url_name = search_result[search_result['item_name']==selected_name]['url_name'].values[0]
+        item_info = get_item_info(url_name)
+        item_orders = get_item_orders(url_name)
+        st.write(f"➖ ⏱️ **Info: {item_info['time'].split()[1]}** ➖ ⏲️ **Orders: {item_orders['time'].split()[1]}** ➖")
