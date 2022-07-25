0d553237b56632a621756be4f5d840cc6b884d54
dearfad <dearfad@sina.com>
2022-07-25 15:45:16 +0800

    update

--- pages/1_Search.py
+++ pages/1_Search.py
@@ -22,7 +22,9 @@ else:
     st.write(f"➖ ⏱️ **Info: {item_info['time'].split()[1]}** ➖ ⏲️ **Orders: {item_orders['time'].split()[1]}** ➖")
     st.write(f"### **{item_info['info']['zh-hans']['item_name']}**")
 
-    price_col, thumb_col, description_col = st.columns([1,2,4])
+    search_col, info_col = st.columns(2)
+
+    price_col, thumb_col, description_col = search_col.columns([1,2,4])
     with price_col:        
         item_price = get_item_price(item_orders['orders'])
         st.metric(label='最低卖价', value=item_price['ingame_lowest_sell_platinum'])
