fa9ec64092b724ed12ec33a565416122bb60d805
dearfad <dearfad@sina.com>
2022-07-25 15:49:59 +0800

    update

--- pages/1_Search.py
+++ pages/1_Search.py
@@ -12,32 +12,30 @@ search_col, info_col = st.columns(2)
 with search_col:
     input_name = st.text_input('模糊搜索：', '')
     search_result = items[items['item_name'].str.contains(input_name.strip(), case=False)]
-    if search_result.empty:
-        st.warning('未找到相关信息...')
-    else:
-        
-        selected_name = st.selectbox('已发现：', search_result['item_name'])
 
-        url_name = search_result[search_result['item_name']==selected_name]['url_name'].values[0]
-        item_info = get_item_info(url_name)
-        item_orders = get_item_orders(url_name)
+if search_result.empty:
+    st.warning('未找到相关信息...')
+else:    
+    selected_name = st.selectbox('已发现：', search_result['item_name'])
+    url_name = search_result[search_result['item_name']==selected_name]['url_name'].values[0]
+    item_info = get_item_info(url_name)
+    item_orders = get_item_orders(url_name)
 
+    with search_col:
         st.write(f"➖ ⏱️ **Info: {item_info['time'].split()[1]}** ➖ ⏲️ **Orders: {item_orders['time'].split()[1]}** ➖")
         st.write(f"### **{item_info['info']['zh-hans']['item_name']}**")
-
-        # price_col, thumb_col, description_col = search_col.columns([1,2,4])
-        # with price_col:        
-        #     item_price = get_item_price(item_orders['orders'])
-        #     st.metric(label='最低卖价', value=item_price['ingame_lowest_sell_platinum'])
-        #     st.metric(label='最高买价', value=item_price['ingame_highest_buy_platinum'])
-        # with thumb_col:
-        #     wiki_link = item_info['info']['zh-hans']['wiki_link']
-        #     if wiki_link:
-        #         st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})]({item_info['info']['zh-hans']['wiki_link']})")
-        #     else:
-        #         st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})")
-        # with description_col:
-        #     st.write(f"![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info['info'].get('ducats', '--')}**")
-        #     st.write(f"📝 {item_info['info']['zh-hans']['description']}")
-        #     st.write(f"[WARFRAME MARKET]({item_url+url_name})")
-    
+    # price_col, thumb_col, description_col = search_col.columns([1,2,4])
+    # with price_col:        
+    #     item_price = get_item_price(item_orders['orders'])
+    #     st.metric(label='最低卖价', value=item_price['ingame_lowest_sell_platinum'])
+    #     st.metric(label='最高买价', value=item_price['ingame_highest_buy_platinum'])
+    # with thumb_col:
+    #     wiki_link = item_info['info']['zh-hans']['wiki_link']
+    #     if wiki_link:
+    #         st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})]({item_info['info']['zh-hans']['wiki_link']})")
+    #     else:
+    #         st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})")
+    # with description_col:
+    #     st.write(f"![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info['info'].get('ducats', '--')}**")
+    #     st.write(f"📝 {item_info['info']['zh-hans']['description']}")
+    #     st.write(f"[WARFRAME MARKET]({item_url+url_name})")
