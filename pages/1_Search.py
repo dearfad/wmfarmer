9daa583caeb6822444b083f18b88ae895347f2df
dearfad <dearfad@sina.com>
2022-07-25 16:01:15 +0800

    update

--- pages/1_Search.py
+++ pages/1_Search.py
@@ -25,10 +25,11 @@ with search_col:
         st.write(f"### **{item_info['info']['zh-hans']['item_name']}**")
         wiki_link = item_info['info']['zh-hans']['wiki_link']
         if wiki_link:
-            st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})]({item_info['info']['zh-hans']['wiki_link']})")
+            # st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})]({item_info['info']['zh-hans']['wiki_link']})")
+            st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['icon']})]({item_info['info']['zh-hans']['wiki_link']})")
         else:
-            st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})")
-        st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['icon']})")
+            # st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})")
+            st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['icon']})")
     # price_col, thumb_col, description_col = search_col.columns([1,2,4])
     # with price_col:        
     #     item_price = get_item_price(item_orders['orders'])
