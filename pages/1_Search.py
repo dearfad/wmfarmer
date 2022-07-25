dc4f65478c95d156610d0481f8e341afe515a128
dearfad <dearfad@sina.com>
2022-07-25 15:59:45 +0800

    update

--- pages/1_Search.py
+++ pages/1_Search.py
@@ -28,8 +28,6 @@ with search_col:
             st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})]({item_info['info']['zh-hans']['wiki_link']})")
         else:
             st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})")
-        
-        st.write(item_info)
         st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['icon']})")
     # price_col, thumb_col, description_col = search_col.columns([1,2,4])
     # with price_col:        
