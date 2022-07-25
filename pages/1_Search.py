60f56d412cf5ff845973912f8e6ac1d0d33b2cd7
dearfad <dearfad@sina.com>
2022-07-25 10:08:39 +0800

    update

--- pages/1_Search.py
+++ pages/1_Search.py
@@ -37,4 +37,6 @@ else:
         st.write(f"![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info['info'].get('ducats', '--')}**")
         st.write(f"📝 {item_info['info']['zh-hans']['description']}")
         st.write(f"[WARFRAME MARKET]({item_url+url_name})")
+    
+    st.write(item_info)
         
