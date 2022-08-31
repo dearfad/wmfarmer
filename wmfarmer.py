ec6868e315e6e83940113d5a55effa5eb3e74a34
dearfad <dearfad@sina.com>
2022-08-31 13:43:42 +0800

    update

--- wmfarmer.py
+++ wmfarmer.py
@@ -42,7 +42,7 @@ if url_name:
 warframe, weapon, mod = st.tabs(["战甲Prime", "武器Prime", "MOD"])
 
 with warframe:
-    col_1, col_2 = st.columns([1,1])
+    col_1, col_2, col_3 = st.columns([1,1,1])
     with col_2:
         warframe_price_df = get_warframe_price()
         warframe_table = "|名 称|套 装|蓝 图|头 部|机 体|系 统|\n|:---:|:---:|:---:|:---:|:---:|:---:|\n"
