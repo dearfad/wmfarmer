74697a34e8ee38a6369a8bb158464b44b683225e
dearfad <dearfad@sina.com>
2022-08-31 09:47:12 +0800

    update

--- wmfarmer.py
+++ wmfarmer.py
@@ -33,22 +33,10 @@ if url_name:
     item_orders = get_item_orders(url_name)
     
     with info_col:
-        # st.write(f"➖ ⏱️ **Info: {item_info['time'].split()[1]}** ➖ ⏲️ **Orders: {item_orders['time'].split()[1]}** ➖")
         st.write(f"#### **{item_info['info']['zh-hans']['item_name']}**")
-        # wiki_link = item_info['info']['zh-hans']['wiki_link']
-        # if wiki_link:
-        #     # st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})]({item_info['info']['zh-hans']['wiki_link']})")
-        #     st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['icon']})]({item_info['info']['zh-hans']['wiki_link']})")
-        # else:
-        #     # st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})")
-        #     st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['icon']})")
         item_price = get_item_price(item_orders['orders'])
-        st.write(f"###### ![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info['info'].get('ducats', '--')}** - [WM]({item_url+url_name}) - [WIKI]({item_info['info']['zh-hans']['wiki_link']})")
+        st.write(f"###### ![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info['info'].get('ducats', '0')}** - [WM]({item_url+url_name}) - [WIKI]({item_info['info']['zh-hans']['wiki_link']})")
         st.write(f"###### 最低卖价: {item_price['ingame_lowest_sell_platinum']}")
         st.write(f"###### 最高买价: {item_price['ingame_highest_buy_platinum']}")
-        # st.metric(label='最低卖价', value=item_price['ingame_lowest_sell_platinum'])
-        # st.metric(label='最高买价', value=item_price['ingame_highest_buy_platinum'])
-        # st.write(f"📝 {item_info['info']['zh-hans']['description']}")
-        # st.write(f"###### [WM]({item_url+url_name})")
 
 tab1, tab2, tab3 = st.tabs(["战甲", "武器", "MOD"])
