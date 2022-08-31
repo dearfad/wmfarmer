076bfb10a9bc61259fb6edb1ae870b45acb2b089
dearfad <dearfad@sina.com>
2022-08-31 09:32:34 +0800

    UPDATE

--- wmfarmer.py
+++ wmfarmer.py
@@ -35,7 +35,7 @@ if url_name:
     with info_col:
         # st.write(f"➖ ⏱️ **Info: {item_info['time'].split()[1]}** ➖ ⏲️ **Orders: {item_orders['time'].split()[1]}** ➖")
         st.write(f"#### **{item_info['info']['zh-hans']['item_name']}**")
-        # wiki_link = item_info['info']['zh-hans']['wiki_link']
+        wiki_link = item_info['info']['zh-hans']['wiki_link']
         # if wiki_link:
         #     # st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})]({item_info['info']['zh-hans']['wiki_link']})")
         #     st.write(f"[![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['icon']})]({item_info['info']['zh-hans']['wiki_link']})")
@@ -43,7 +43,7 @@ if url_name:
         #     # st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['thumb']})")
         #     st.write(f"![{item_info['info']['zh-hans']['item_name']}]({assets_url+item_info['info']['icon']})")
         item_price = get_item_price(item_orders['orders'])
-        st.write(f"###### ![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info['info'].get('ducats', '--')}** - [WM]({item_url+url_name}) - ")
+        st.write(f"###### ![ducats](https://warframe.market/static/build/resources/images/icons/Ducats.b2f626d13cd31d84117a.png) **{item_info['info'].get('ducats', '--')}** - [WM]({item_url+url_name}) - [WIKI]{wiki_link}")
         st.write(f"###### 最低卖价: {item_price['ingame_lowest_sell_platinum']}")
         st.write(f"###### 最高买价: {item_price['ingame_highest_buy_platinum']}")
         # st.metric(label='最低卖价', value=item_price['ingame_lowest_sell_platinum'])
