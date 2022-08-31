3260fb6a342aa4f93b484104ec8380580c9f9e69
dearfad <dearfad@sina.com>
2022-08-31 12:44:52 +0800

    update

--- scripts/core.py
+++ scripts/core.py
@@ -62,7 +62,7 @@ def get_weapon_price():
     weapon_prime_list = ['burston','tenora','vectis','braton']
     weapon_prime_set_list = ['set', 'blueprint', 'receiver', 'stock', 'barrel']
 
-    weapon_price_df = pd.DataFrame(data=np.zeros((len(weapon_prime_list),len(weapon_prime_set_list)+1), dtype = int), index=weapon_prime_list, columns=weapon_prime_set_list+'item_name')
+    weapon_price_df = pd.DataFrame(data=np.zeros((len(weapon_prime_list),len(weapon_prime_set_list)+1), dtype = int), index=weapon_prime_list, columns=weapon_prime_set_list.append('item_name'))
 
     progress = st.empty()
 
